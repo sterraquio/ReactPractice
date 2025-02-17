52159eff7ebae3ec95bfdf8785a431c054e3e6ef
Andrick Buitrago <sterraquio@hotmail.com>
2025-02-17 12:00:49 -0500

    Vercel instalado

--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,6 +1,9 @@
 import "./Quiz.css";
+import { useCallback } from "react";
+import useQuizStore from "../../stores/use-quiz-store";
 
 const Quiz = () => {
+  // const {quiz, incrementQuizProgress}
   return <div><h1>
     Quiz
     </h1>
